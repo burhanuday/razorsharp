beeb032b3c0982389b1e3f7829916ef788093ef3
Burhanuddin Udaipurwala <burhanuddin.u@razorpay.com>
2023-02-25 12:14:17 +0530

    feat: add icon support in button

--- src/code/blade/components/button.ts
+++ src/code/blade/components/button.ts
@@ -1,12 +1,17 @@
 import { BladeComponentInstanceNode, BladeTextNode } from "~/code/types/Blade";
 import { jsxValue } from "../utils/attributes";
 import { component } from "../utils/component";
+import {
+  convertFigmaIconNameToBladeIconName,
+  isIconInstance,
+} from "../utils/iconUtils";
 import { traverseNodeTree } from "../utils/traverseNodeTree";
 
 const defaultValues: Record<string, string> = {
   variant: "primary",
   size: "medium",
   isFullWidth: "false",
+  iconPosition: "left",
 };
 
 const transformButtonVariant = (variant: string): string => {
@@ -20,18 +25,52 @@ export const transformButton = (
 
   const size = componentProperties.size.value;
   const variant = componentProperties.variant.value;
-
-  const props = {
-    size: jsxValue(size).toLowerCase(),
-    variant: transformButtonVariant(jsxValue(variant)),
-    isFullWidth: jsxValue(componentProperties.isFullWidth.value),
-  };
+  let iconName = "";
+  let iconPosition = "";
 
   const labelTextNode = traverseNodeTree(
     bladeComponentInstance,
     (node) => node.layerName === "Text" && node.type === "TEXT"
   );
   const children = (labelTextNode as BladeTextNode)?.characters;
+
+  const iconLeftNode = traverseNodeTree(
+    bladeComponentInstance,
+    (bladeNode) => bladeNode.layerName === "Icon Left"
+  );
+
+  if (
+    iconLeftNode !== null &&
+    isIconInstance(iconLeftNode as BladeComponentInstanceNode)
+  ) {
+    iconName = convertFigmaIconNameToBladeIconName(
+      (iconLeftNode as BladeComponentInstanceNode)?.name || "unidentified-icon"
+    );
+    iconPosition = "left";
+  }
+
+  const iconRightNode = traverseNodeTree(
+    bladeComponentInstance,
+    (bladeNode) => bladeNode.layerName === "Icon Right"
+  );
+
+  if (
+    iconRightNode !== null &&
+    isIconInstance(iconRightNode as BladeComponentInstanceNode)
+  ) {
+    iconName = convertFigmaIconNameToBladeIconName(
+      (iconRightNode as BladeComponentInstanceNode)?.name || "unidentified-icon"
+    );
+    iconPosition = "right";
+  }
+
+  const props = {
+    size: jsxValue(size).toLowerCase(),
+    variant: transformButtonVariant(jsxValue(variant)),
+    isFullWidth: jsxValue(componentProperties.isFullWidth.value),
+    iconName,
+    iconPosition,
+  };
 
   return component("Button", {
     props,
