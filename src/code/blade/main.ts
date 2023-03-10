ff4dc116c9b4a0378ecf264e27929ce11d57e0cc
Burhanuddin Udaipurwala <burhanuddin.u@razorpay.com>
2023-03-10 09:21:20 +0530

    refactor: main refactored

--- src/code/blade/main.ts
+++ src/code/blade/main.ts
@@ -1,130 +1,17 @@
 import {
+  BladeNode,
   BladeComponentInstanceNode,
   BladeFrameNode,
-  BladeGroupNode,
-  BladeNode,
   BladeTextNode,
+  BladeGroupNode,
 } from "../types/Blade";
 import { TransformFunctionReturnType } from "../types/TransformFunction";
-import { transformFrameOrGroup } from "./components/box";
-import { transformButton } from "./components/button";
-import { transformIcon } from "./components/icon";
 import {
-  transformHeading,
-  transformText,
-  transformTitle,
-  transformTextNode,
-  transformCode,
-} from "./components/typography";
-import {
-  transformOtpInput,
-  transformPasswordInput,
-  transformSelectInput,
-  transformTextArea,
-  transformTextInput,
-} from "./components/input";
-import { isIconInstance } from "./utils/iconUtils";
-import { transformBadge } from "./components/badge";
-import { transformLink } from "./components/link";
-import {
-  transformCheckbox,
-  transformCheckboxGroup,
-} from "./components/checkbox";
-import { transformRadio, transformRadioGroup } from "./components/radio";
-import { transformAlert } from "./components/alert";
-import { transformSpinner } from "./components/spinner";
-import { transformCounter } from "./components/counter";
-import { transformIconButton } from "./components/iconButton";
-import { transformIndicator } from "./components/indicator";
-import { transformProgressBar } from "./components/progressBar";
-
-const getUnknownComponentOutput = (
-  component: BladeComponentInstanceNode
-): TransformFunctionReturnType => {
-  return {
-    component: `
-{/* 
-  ${component.name} is either not supported yet or not a part of Blade.
-  <${component.name} /> 
-*/}`,
-  };
-};
-
-const generateBladeComponentInstanceCode = (
-  bladeComponentInstance: BladeComponentInstanceNode
-): TransformFunctionReturnType => {
-  // check if component instance is an icon
-  const isIcon = isIconInstance(bladeComponentInstance);
-  if (isIcon) return transformIcon(bladeComponentInstance);
-
-  // handle all other components
-  switch (bladeComponentInstance.name) {
-    case "Button":
-      return transformButton(bladeComponentInstance);
-    case "Text Input":
-      return transformTextInput(bladeComponentInstance);
-    case "Badge":
-      return transformBadge(bladeComponentInstance);
-    case "Link":
-      return transformLink(bladeComponentInstance);
-    case "Checkbox":
-      return transformCheckbox(bladeComponentInstance);
-    case "Title":
-      return transformTitle(bladeComponentInstance);
-    case "Heading":
-      return transformHeading(bladeComponentInstance);
-    case "Text":
-      return transformText(bladeComponentInstance);
-    case "Code":
-      return transformCode(bladeComponentInstance);
-    case "Radio-Button":
-      return transformRadio(bladeComponentInstance);
-    case "Checkbox-Group":
-      return transformCheckboxGroup(bladeComponentInstance);
-    case "Radio-Group":
-      return transformRadioGroup(bladeComponentInstance);
-    case "Alert":
-      return transformAlert(bladeComponentInstance);
-    case "Spinner":
-      return transformSpinner(bladeComponentInstance);
-    case "TextArea Input":
-      return transformTextArea(bladeComponentInstance);
-    case "Password Input":
-      return transformPasswordInput(bladeComponentInstance);
-    case "OTP Input":
-      return transformOtpInput(bladeComponentInstance);
-    case "Counter":
-      return transformCounter(bladeComponentInstance);
-    case "IconButton":
-      return transformIconButton(bladeComponentInstance);
-    case "Indicators":
-      return transformIndicator(bladeComponentInstance);
-    case "ProgressBar":
-      return transformProgressBar(bladeComponentInstance);
-    case "Select Input":
-      return transformSelectInput(bladeComponentInstance);
-    default:
-      return getUnknownComponentOutput(bladeComponentInstance);
-  }
-};
-
-const generateBladeFrameCode = (
-  bladeNode: BladeFrameNode
-): TransformFunctionReturnType => {
-  return transformFrameOrGroup(bladeNode, generateBladeCode);
-};
-
-const generateTextNodeCode = (
-  bladeNode: BladeTextNode
-): TransformFunctionReturnType => {
-  return transformTextNode(bladeNode);
-};
-
-const generateGroupNodeCode = (
-  bladeNode: BladeGroupNode
-): TransformFunctionReturnType => {
-  return transformFrameOrGroup(bladeNode, generateBladeCode);
-};
+  generateBladeComponentInstanceCode,
+  generateBladeFrameCode,
+  generateTextNodeCode,
+  generateGroupNodeCode,
+} from "./components";
 
 export const generateBladeCode = ({
   bladeNodes,
